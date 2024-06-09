c31d7894d2053f7d928663dc0a38cda1e1091fcf
andrey_mazo <andreymazo@mail.ru>
2024-06-09 09:56:02 +0300

    update

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -1,19 +1,19 @@
 from django.urls import include, path
 
 from bulletin.apps import BulletinConfig
-from bulletin.views import (confirm_code, create_profile, get_new_code, home,
-                            log_in, log_out, sign_up, verify_code)
+from bulletin.views import (confirm_code, create_profile, home,
+                            log_in,  sign_up, verify_code)
 
 app_name = BulletinConfig.name
 
 url_v1 = [
     path("verify_code/", verify_code, name="verify_code"),
     path("log_in/", log_in, name="log_in"),
-    path("log_out/", log_out, name="log_out"),
+    # path("log_out/", log_out, name="log_out"),
     path("create_profile/", create_profile, name="create_profile"),
     path("confirm_code/", confirm_code, name="confirm_code"),
     path("sign_up/", sign_up, name="sign_up"),
-    path("get_new_code/", get_new_code, name="get_new_code")
+    # path("get_new_code/", get_new_code, name="get_new_code")
 ]
 
 

ac6cfe1ebf5f82c44b96e8f5a89da773d4daa14b
andrey_mazo <andreymazo@mail.ru>
2024-06-20 16:43:07 +0300

    new models + IP with create under signal

--- ad/models.py
+++ ad/models.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey
@@ -33,3 +34,65 @@ class Category(MPTTModel):
     def __str__(self):
         """Строковое представление категории."""
         return self.name
+
+
+class Advertisement(models.Model):
+    title = models.CharField(_("Название"),max_length=175)
+    description = models.CharField(_("Описание"), max_length=2000)
+    profile = models.ForeignKey("users.Profile", on_delete=models.CASCADE, **NULLABLE)
+    category = models.ForeignKey("ad.Category", on_delete=models.CASCADE, **NULLABLE)
+    created = models.DateTimeField(auto_now=True)
+    changed = models.DateTimeField(auto_now_add=True)
+    moderation = models.BooleanField(_("Модерация"), default=False)
+
+
+from django.db.models.signals import post_save
+
+class IP(models.Model):
+    ip = models.CharField(max_length=100)
+    profile = models.ForeignKey("users.Profile", on_delete=models.CASCADE, **NULLABLE)
+    advertisement = models.ForeignKey("ad.Advertisement", on_delete=models.CASCADE, **NULLABLE)
+    
+    # def initialize(self, *args, **kwargs):
+    #     self.ip = kwargs['ip']
+    #     self.profile = kwargs['profile']
+    #     self.advertisement = kwargs['advertisement']
+
+    # @classmethod
+    # def create(cls, *args, **kwargs):
+    #     # LOGICS
+    #     print('--------------------------------------------------')
+
+    #     self = cls()
+    #     self.initialize(*args, **kwargs)
+    #     return self
+
+    @classmethod
+    def post_create(cls, sender, instance, created, *args, **kwargs):
+        print('======================instance.id', instance.profile_id)
+        queryset = IP.objects.all().filter(profile_id=instance.profile_id)
+        # print('--------------------------------------------------', instance.__dict__, sender, queryset)
+      
+        if len(queryset)>2:
+            # print('--------------------------------------------------', sender, instance, queryset)
+            queryset.exclude(pk__in=queryset.values_list("pk")[:2]).delete()
+        return
+
+post_save.connect(IP.post_create, sender=IP)
+
+
+class Images(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.FileField(_("Фотография"), upload_to="media/images")
+    profile = models.ForeignKey("users.Profile", on_delete=models.CASCADE, **NULLABLE)
+    advertisement = models.ForeignKey("ad.Advertisement", on_delete=models.CASCADE, **NULLABLE)
+    created = models.DateTimeField(auto_now=True)
+    changed = models.DateTimeField(auto_now_add=True)
+
+
+class Documents(models.Model):
+    title = models.CharField(max_length=150)
+    document = models.FileField(_("Документ"), upload_to="media/documents")
+    profile = models.ForeignKey("users.Profile", on_delete=models.CASCADE, **NULLABLE)
+    advertisement = models.ForeignKey("ad.Advertisement", on_delete=models.CASCADE, **NULLABLE)
+    created = models.DateTimeField(auto_now=True)
--- bulletin/management/commands/1.py
+++ bulletin/management/commands/1.py
@@ -1,10 +1,11 @@
 
 import os
 from django.core.management import BaseCommand
+from ad.models import IP, Advertisement
 from config.settings import BASE_DIR, MEDIA_ROOT, MEDIA_URL
 # from django.conf import settings
 from config import settings
-from users.models import CustomUser
+from users.models import CustomUser, Profile
 
  #note settings is an object , hence you cannot import its contents
 
@@ -18,11 +19,27 @@ from users.models import CustomUser
 
  
 def ff():
-    user = CustomUser.objects.get(email='andreymazo@mail.ru')
-    otc = user.onetimecodes
+    profile_queryset = Profile.objects.all()
+    # # ip_queryset = IP.objects.all()
+    # # advert_list = [("ad1", "ad_disr_1", profile_queryset.get(id=5)), ("ad2","ad_disr_2", profile_queryset.get(id=6)), ("ad3","ad_disr_3", profile_queryset.get(id=6))]
+    # ip_list = ["123", "234", "456"]
+    # # for i in advert_list:
+    # #     # print(i[0])
+    # #     Advertisement.objects.create(title=i[0], description=i[1], profile=i[2])
+    # # print(Advertisement.objects.all().values_list('profile_id'))
+    advertizement_queryset = Advertisement.objects.all()
+    # # print(Advertisement.objects.all())
+    # for i, j, ii in zip(ip_list, profile_queryset, advertizement_queryset):
+    #     IP.objects.create(ip=i, profile=j, advertisement=ii)
+    #########################
+    advertisement = advertizement_queryset.get(id=50)
+    profile = profile_queryset.get(id=5) 
+    IP.objects.create(ip="123", profile=profile, advertisement=advertisement)
+    # user = CustomUser.objects.get(email='andreymazo@mail.ru')
+    # otc = user.onetimecodes
     # print(otc)
-    DJANGO_SETTINGS_MODULE=settings
-    print(os.environ['DJANGO_SETTINGS_MODULE'])
+    # DJANGO_SETTINGS_MODULE=settings
+    # print(os.environ['DJANGO_SETTINGS_MODULE'])
     # lang_dict = dict(settings.LANGUAGES)
     # user = CustomUser.objects.all().get(pk=17)
     # print(CodeCustomuser.objects.filter(customuser_id=user.id).exists())

2695ccb2bf894ffcd45ba526a8aff1bc41968073
andrey_mazo <andreymazo@mail.ru>
2024-06-23 01:28:26 +0300

    Auto model in ad app

--- ad/models.py
+++ ad/models.py
@@ -4,7 +4,8 @@ from django.utils.translation import gettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey
 from django.db.models.signals import post_save
 from config.constants import MAX_LEN_NAME_CATEGORY
-
+import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 NULLABLE = {'blank': True, 'null': True}
 
@@ -44,6 +45,46 @@ class Advertisement(models.Model):
     created = models.DateTimeField(auto_now=True)
     changed = models.DateTimeField(auto_now_add=True)
     moderation = models.BooleanField(_("Модерация"), default=False)
+
+    class Meta:
+        abstract = True
+
+BY_MILEAGE = [("ALL", "ALL"), ("NEW", "NEW"), ("WM", "With Mileage"),]
+
+BY_TRANSMISSION = [("A", "AUTO"), ("R", "ROBOT"), ("V", "VARIATOR"), ("M", "MECHANICAL"),]
+
+BY_DRIVE = [("FWD", "FRONT_WHEEL_DRIVE"), ("RWD", "REAR_WHEEL_DRIVE"), ("4WD", "VARIATOR"),]
+
+BY_TYPE = [("SD", "SEDAN"), ("UN", "UNIVERSAL"), ("OR", "OFF_ROAD"), ("HTB", "HATCHBACK")]
+
+BY_COLOUR = [("BLACK", "BLACK"), ("WHITE", "WHITE"), ("GREEN", "GREEN"), ("GREY", "GREY"), ("RED", "RED"), ("ORANGE", "ORANGE"), ("BEIGE", "BEIGE"), ("BROWN", "BROWN")]
+
+BY_FUEL = [("PTR", "PETROL"), ("GAS", "GAS"), ("HYBRID", "HYBRID"), ("ELECTRIC", "ELECTRIC"), ("DIESEL", "DIESEL")]
+
+def current_year():
+    return datetime.date.today().year
+
+def max_value_current_year(value):
+    return MaxValueValidator(current_year())(value)
+   
+class Auto(Advertisement):
+    by_mileage = models.CharField(choices=BY_MILEAGE)
+    brand = models.CharField(_("brand"),max_length=100)
+    model = models.CharField(_("model"), max_length=100)
+    price = models.CharField(_("price"), max_length=100)
+    year = models.PositiveIntegerField(_('year'), default=current_year(), validators=[MinValueValidator(1984), 
+                                                                                      max_value_current_year])
+    mileage = models.IntegerField()
+    transmission = models.CharField(choices=BY_TRANSMISSION, **NULLABLE)
+    by_wheel_drive = models.CharField(choices=BY_DRIVE, **NULLABLE)
+    engine_capacity = models.IntegerField(_("engine capacity"), validators=[MinValueValidator(0), MaxValueValidator(10000)], **NULLABLE)
+    engine_power = models.IntegerField(_("engine power in horse power"), validators=[MinValueValidator(0), MaxValueValidator(900)], **NULLABLE) 
+    fuel_consumption = models.IntegerField(_("fuel consumption per 100 km"), validators=[MinValueValidator(1), MaxValueValidator(40)], **NULLABLE)
+    location = models.CharField(_("location"), max_length=160, **NULLABLE)
+    type = models.CharField(choices=BY_TYPE, **NULLABLE)
+    colour = models.CharField(choices=BY_COLOUR, **NULLABLE)
+    fuel = models.CharField(choices=BY_FUEL, **NULLABLE)
+    add_parametres =  models.CharField(_("additional"), max_length=150, **NULLABLE)
 
 
 class IP(models.Model):

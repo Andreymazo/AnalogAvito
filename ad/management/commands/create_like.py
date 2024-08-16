28ce645ba5b513e60cb944d734e8f9abd8c63c23
andrey_mazo <andreymazo@mail.ru>
2024-08-16 21:07:04 +0300

    create_images, create views, create_profile, creaye_likes + get_model_fm_category 439str ad/view

--- ad/management/commands/create_like.py
+++ ad/management/commands/create_like.py
@@ -6,11 +6,10 @@ from django.contrib.contenttypes.models import ContentType
 
  
 def ff():
-    user_id = CustomUser.objects.get(id=14).id
-    obj = Car.objects.get(pk=5)
+    user_id = CustomUser.objects.first().id
+    obj = Car.objects.first()
     Like.objects.create(is_liked=False, user_id=user_id, content_type=ContentType.objects.get_for_model(obj), object_id=obj.id)
-    #obj = self.model.objects.get(pk=pk)
-#   content_type=ContentType.objects.get_for_model(obj), object_id=obj.id, user=request.user
+  
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
--- ad/management/commands/create_views.py
+++ ad/management/commands/create_views.py
@@ -7,11 +7,12 @@ from django.contrib.contenttypes.models import ContentType
 
 def ff():
     """Создаем просмотр на объявление автомобиль юзера по ip"""
-    ip_instance = IP.objects.first()
+    ip_instance = IP.objects.first()# решили пока без привязки к айпи делать
     car_instance = Car.objects.first()
     content_type = ContentType.objects.get_for_model(car_instance)
-    view_instance = Views.objects.get_or_create(content_type=content_type, object_id=car_instance.id, ip=ip_instance)
-   
+    # view_instance = Views.objects.get_or_create(content_type=content_type, object_id=car_instance.id, ip=ip_instance)
+    view_instance = Views(content_object=car_instance)
+    view_instance.save()
 class Command(BaseCommand):
 # kill -9 $(lsof -t -i:8000)
     def handle(self, *args, **options):

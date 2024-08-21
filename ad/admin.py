cc23dbc564760e7151c45b71eefb9fde095f5f1c
Oleg Smirnov <oleg.smirnoff505@yandex.ru>
2024-08-22 01:16:40 +0500

    fix

--- ad/admin.py
+++ ad/admin.py
@@ -1,6 +1,7 @@
-from django.contrib.admin import ModelAdmin, register
+from django.contrib.admin import ModelAdmin, register, display
+from django.utils.safestring import mark_safe
 
-from ad.models import Car, Category, Images, Views
+from ad.models import Car, Category, Images, Views, Like, Documents
 from modeltranslation.admin import TranslationAdmin
 
 from modeltranslation.admin import TranslationAdmin
@@ -25,3 +26,19 @@ class CategoryAdmin(ModelAdmin):
 @register(Views)
 class ViewsAdmin(ModelAdmin):
     list_display = ("profile",)
+
+@register(Like)
+class LikeAdmin(ModelAdmin):
+    list_display = ('user', 'content_type', 'content_object',)
+    list_display_links = ('user',)
+
+@register(Documents)
+class DocumentsAdmin(ModelAdmin):
+    list_display = ('title', 'profile', 'auto', 'created', 'document_image',)
+    list_display_links = ('title',)
+    list_filter = ('profile',)
+    ordering = ('-created',)
+
+    @display(description='Миниатюра')
+    def document_image(self, documents: Documents):
+        return mark_safe(f"<img src='{documents.document.url}' width=100>")

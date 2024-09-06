5eb36564b22c3d7477320dfd21753108d083561f
andrey_mazo <andreymazo@mail.ru>
2024-09-06 14:39:56 +0300

    user cant inherit from smth, contenttypes fields removed

--- bulletin/management/commands/create_super_user.py
+++ bulletin/management/commands/create_super_user.py
@@ -2,17 +2,28 @@
 from django.core.management import BaseCommand
 from users.models import CustomUser
 from users.managers import CustomUserManager
-
+from django.contrib.contenttypes.models import ContentType
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # create_superuser = CustomUserManager.create_superuser
-        user , created = CustomUser.objects.update_or_create(username="admin", email = 'andreymazoo@mail.ru', password = 'qwert123asd', is_superuser=True, is_staff=True, is_active=True)
+        ct = ContentType.objects.get(model='customuser')
+        ct_class = ct.model_class()
+        ct_instance = ct_class()
+        ct_instance.username = "admin"
+        ct_instance.email = 'andreymazoo@mail.ru'
+        ct_instance.password = 'qwert123asd'
+        ct_instance.object_id = ct_class.id
+        ct_instance.save()
+
+        # user_type = ContentType.objects.get(app_label = "users", model = "customuser")
+        # user_model_class = user_type.model_class()
+        # specific_user = user_type.get_object_for_this_type(username = 'Guido')
+        # content_type = ContentType.objects.get_for_model(CustomUser)
+        # user  = CustomUser(username="admin", email = 'andreymazoo@mail.ru', password = 'qwert123asd', \
+        #                             is_superuser=True, is_staff=True, is_active=True, content_type=content_type, object_id = ct_instance.id())
+        # user.set_password('qwert123asd')
+        # # user.username='admin'
         # user.save()
-        # user = CustomUser.objects.create_superuser(username="admin", email = 'andreymazoo@mail.ru', password="qwert123asd")
-        user.set_password('qwert123asd')
-        # user.username='admin'
-        user.save()
         
 #         {"username": "andreymazo@mail.ru",
 # "password":"qwert123asd"}
